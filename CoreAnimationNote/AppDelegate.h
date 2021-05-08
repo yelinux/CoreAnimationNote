5310b80f8076c21bbf0f4266c305d9666dd8780d
chenyehong <ye_linux@126.com>
2021-05-08 10:51:29 +0800

    Initial Commit

---
+++ CoreAnimationNote/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CoreAnimationNote
+//
+//  Created by chenyehong on 2021/5/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CoreAnimationNote/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CoreAnimationNote
+//
+//  Created by chenyehong on 2021/5/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
